7044d55725c257a487811a2c9964ea790f5ec32a
mkovarky <mikhail.kovarsky@gmail.com>
2020-02-20 12:50:48 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket_cost = 1001;
+        int bonus_miles = (int) (ticket_cost * 0.05);
+        System.out.println(bonus_miles);
+    }
+}
