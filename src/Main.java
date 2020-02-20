ac666ca2a2244cb0a669b96cb2d213b640739e43
mkovarky <mikhail.kovarsky@gmail.com>
2020-02-20 13:34:10 +0300

    Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket_cost = 1001;
-        int bonus_miles = (int) (ticket_cost * 0.05);
+        // Стоимость билета в копейках
+        int ticket_cost = 7900_00;
+        int bonus_miles = (int) (ticket_cost*0.05/100);
         System.out.println(bonus_miles);
     }
 }
